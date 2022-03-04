e9bd8bd295abb5962278c844a575323f205f2716
MarchiyGV <Marchiy.Georgiy@yandex.ru>
2022-03-04 12:58:12 +0300

    c

--- plot_energy.sh
+++ plot_energy.sh
@@ -2,7 +2,8 @@
 
 prefix=""
 suffix=""
-
+outsuffix="_cpu90"
+y="!"
 while [ True ]; do
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
     echo "-n --name"
@@ -13,6 +14,9 @@ elif [ "$1" = "--prefix" -o "$1" = "-p" ]; then
 elif [ "$1" = "--suffix" -o "$1" = "-s" ]; then
     suffix=$2
     shift 2
+elif [ "$1" = "--outsuffix" -o "$1" = "-c" ]; then
+    outsuffix=$2
+    shift 2
 else
     break
 fi
@@ -21,5 +25,6 @@ done
 for name in $@
 do 
 echo "${prefix}${name}${suffix}"
-echo `grep -e '!' ${prefix}${name}${suffix}.out`
+echo `grep -e '${x}' ${prefix}${name}${suffix}.in`
+echo `grep -e '${y}' ${prefix}${name}${suffix}${outsuffix}.out`
 done
