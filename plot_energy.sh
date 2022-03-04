b0a96f4250e08675a849950bf92a6987a98478db
MarchiyGV <Marchiy.Georgiy@yandex.ru>
2022-03-04 12:49:27 +0300

    c

--- plot_energy.sh
+++ plot_energy.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
+
+prefix=""
+suffix=""
+
 while [ True ]; do
 if [ "$1" = "--help" -o "$1" = "-h" ]; then
     echo "-n --name"
     exit 0;
-elif [ "$1" = "--name" -o "$1" = "-n" ]; then
-    name=$2
+elif [ "$1" = "--prefix" -o "$1" = "-p" ]; then
+    prefix=$2
+    shift 2
+elif [ "$1" = "--suffix" -o "$1" = "-s" ]; then
+    suffix=$2
     shift 2
 else
     break
 fi
 done
 
-for prefix in $@
+for name in $@
 do 
-echo $prefix
-echo `grep -e '!' ${prefix}.out`
+echo "${prefix}${name}${suffix}"
+echo `grep -e '!' ${prefix}${name}${suffix}.out`
 done
